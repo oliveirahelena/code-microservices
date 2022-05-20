633f0bcc01ebdbcf2f2be071eeaa0fb43ce6b340
Helena Oliveira <helena@informach.com.br>
2022-05-20 18:57:36 -0300

    initial config

---
+++ micro-videos/src/__seedwork/domain/validators.py
@@ -0,0 +1,4 @@
+from typing import Dict, List
+
+
+ErrorFields = Dict[str, List[str]]
---
+++ micro-videos/src/category/domain/entities.py
@@ -0,0 +1,16 @@
+from datetime import datetime
+from dataclasses import dataclass, field
+from typing import Optional
+
+from __seedwork.domain.entities import Entity
+
+
+@dataclass(kw_only=True, frozen=True)
+class Category(Entity):
+
+    name: str
+    description: Optional[str] = None
+    is_active: Optional[bool] = True
+    # pylint: disable=unnecessary-lambda
+    created_at: Optional[datetime] = field(
+        default_factory=lambda: datetime.now())
